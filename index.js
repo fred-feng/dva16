0999ad4ba74d754c881b02a0b47c7904ce6ea46c
fred <fred.feng@stgame.cn>
2020-05-06 18:21:24 +0800

    增加requestFile

--- index.js
+++ index.js
@@ -1,97 +1,92 @@
 /**
  * Created by Fred(qq:24242811) on 2018/8/18.
  */
-import create from "zustand";
-import axios from "axios";
-import produce from "immer";
+import create from 'zustand'
+import axios from 'axios'
+import produce from 'immer'
 
 /* 参考dva接口绑定 */
-const config = {};
-const store = {};
-const immer = (config) => (set, get, api) =>
-  config((fn) => set(produce(fn)), get, api);
+const config = {}
+const store = {}
+const immer = (config) => (set, get, api) => config((fn) => set(produce(fn)), get, api)
 const wrapPromise = (promise) => {
-  let status = "pending";
-  let result;
+  let status = 'pending'
+  let result
   let suspender = promise.then(
     (r) => {
-      status = "success";
-      result = r;
+      status = 'success'
+      result = r
     },
     (e) => {
-      status = "error";
-      result = e;
+      status = 'error'
+      result = e
     }
-  );
+  )
   return {
     read() {
-      if (status === "pending") {
-        throw suspender;
-      } else if (status === "error") {
-        throw result;
-      } else if (status === "success") {
-        return result;
+      if (status === 'pending') {
+        throw suspender
+      } else if (status === 'error') {
+        throw result
+      } else if (status === 'success') {
+        return result
       }
-    },
-  };
-};
+    }
+  }
+}
 export function initDva(models, { printLog = false, useImmer = true } = {}) {
-  Object.assign(config, { printLog, useImmer });
+  Object.assign(config, { printLog, useImmer })
   for (let { namespace, state, reducers, effects } of models) {
-    bindModel({ namespace, state, reducers, effects });
+    bindModel({ namespace, state, reducers, effects })
   }
 }
 export function bindModel({ namespace, state, reducers, effects }) {
-  let vo = { suspense: {}, reducers, effects };
+  let vo = { suspense: {}, reducers, effects }
   const exec = (set, get) => {
-    vo.set = set;
-    vo.get = get;
+    vo.set = set
+    vo.get = get
     return {
       ...state,
       suspense: (type, pendingWithoutPromise = true) => {
-        config.printLog && console.log("[suspense]", namespace, type);
+        config.printLog && console.log('[suspense]', namespace, type)
         if (vo.suspense[type]) {
-          vo.suspense[type].read();
+          vo.suspense[type].read()
         } else if (pendingWithoutPromise) {
-          throw new Promise((res) => res).then();
+          throw new Promise((res) => res).then()
         }
-      },
-    };
-  };
-  vo.zustand = config.useImmer ? create(immer(exec)) : create(exec);
-  store[namespace] = vo;
-  config.printLog && console.log("[model]", namespace);
+      }
+    }
+  }
+  vo.zustand = config.useImmer ? create(immer(exec)) : create(exec)
+  store[namespace] = vo
+  config.printLog && console.log('[model]', namespace)
 }
 export function useStore(namespace) {
-  return store[namespace] ? { ...store[namespace].zustand[0]() } : null;
+  return store[namespace] ? { ...store[namespace].zustand[0]() } : null
 }
 export function dispatch(namespace, type, payload) {
-  config.printLog && console.log("[dispatch]", namespace, type, payload);
-  let { reducers, effects } = store[namespace];
+  config.printLog && console.log('[dispatch]', namespace, type, payload)
+  let { reducers, effects } = store[namespace]
   if (reducers[type]) {
-    reducer(namespace, type, payload);
+    reducer(namespace, type, payload)
   } else if (effects[type]) {
-    effect(namespace, type, payload);
+    effect(namespace, type, payload)
   } else {
-    console.error(
-      `dispatch[${type}] function not exsits in models[${namespace}]`
-    );
+    console.error(`dispatch[${type}] function not exsits in models[${namespace}]`)
   }
 }
 export function reducer(namespace, type, payload) {
-  config.printLog && console.log("[reducer]", namespace, type, payload);
-  let { set, get, reducers } = store[namespace];
+  config.printLog && console.log('[reducer]', namespace, type, payload)
+  let { set, get, reducers } = store[namespace]
   if (reducers[type]) {
-    set(() => reducers[type](get(), { payload }));
+    set(() => reducers[type](get(), { payload }))
   } else {
-    console.error(
-      `reducers[${type}] function not exsits in models[${namespace}]`
-    );
+    console.error(`reducers[${type}] function not exsits in models[${namespace}]`)
   }
 }
 export function effect(namespace, type, payload) {
-  config.printLog && console.log("[effect]", namespace, type, payload);
-  let { set, get, reducers, effects, suspense } = store[namespace];
+  config.printLog && console.log('[effect]', namespace, type, payload)
+  let { set, get, reducers, effects, suspense } = store[namespace]
   if (effects[type]) {
     suspense[type] = wrapPromise(
       effects[type](
@@ -100,105 +95,103 @@ export function effect(namespace, type, payload) {
           call: (func, params) => func(params),
           reducer: (...args) => {
             if (args.length <= 2) {
-              args.unshift(namespace);
+              args.unshift(namespace)
             }
-            reducer(...args);
+            reducer(...args)
           },
           effect: (...args) => {
             if (args.length <= 2) {
-              args.unshift(namespace);
+              args.unshift(namespace)
             }
-            effect(...args);
+            effect(...args)
           },
-          select: (namespace) => store[namespace].get(),
+          select: (namespace) => store[namespace].get()
         }
       )
-    );
+    )
   } else {
-    console.error(
-      `effects[${type}] function not exsits in models[${namespace}]`
-    );
+    console.error(`effects[${type}] function not exsits in models[${namespace}]`)
   }
 }
 
 /* restful + json + jwt基本网络库 */
-const requstParams = { serverHome: null, errorHanlder: null, extraHeaders: {} };
+const requstParams = { serverHome: null, errorHanlder: null, extraHeaders: {} }
 export function initRequest(serverHome, errorHanlder) {
   if (requstParams) {
-    requstParams.serverHome = serverHome;
-    requstParams.errorHanlder = errorHanlder;
+    requstParams.serverHome = serverHome
+    requstParams.errorHanlder = errorHanlder
   }
 }
 export function bindHeader(key, value) {
-  requstParams.extraHeaders[key] = value;
+  requstParams.extraHeaders[key] = value
 }
 export function bindJWTToken(token) {
-  requstParams.extraHeaders["Authorization"] = token
-    ? `Bearer ${token}`
-    : undefined;
+  requstParams.extraHeaders['Authorization'] = token ? `Bearer ${token}` : undefined
 }
 export function requestGet(url, body) {
-  return request(url, { method: "GET", body });
+  return request(url, { method: 'GET', body })
 }
 export function requestDelete(url) {
-  return request(url, { method: "DELETE" });
+  return request(url, { method: 'DELETE' })
 }
 export function requestPost(url, body) {
-  return request(url, { method: "POST", body });
+  return request(url, { method: 'POST', body })
 }
 export function requestPatch(url, body) {
-  return request(url, { method: "PATCH", body });
+  return request(url, { method: 'PATCH', body })
 }
 export function requestPut(url, body) {
-  return request(url, { method: "PUT", body });
+  return request(url, { method: 'PUT', body })
 }
-function request(url, options) {
+export function requestFile(url, file) {
+  let body = new FormData()
+  body.append('file', file)
+  return request(url, { method: 'POST', body }, 'application/form-data')
+}
+function request(url, options, ContentType = null) {
   return new Promise((resolve, reject) => {
-    let { method, body } = options;
+    let { method, body } = options
     // 添加url前缀
-    if (url.indexOf("https://") === -1 && url.indexOf("http://") === -1) {
-      url =
-        requstParams.serverHome +
-        (url.indexOf("/") === 0 ? url.substr(1) : url);
+    if (url.indexOf('https://') === -1 && url.indexOf('http://') === -1) {
+      url = requstParams.serverHome + (url.indexOf('/') === 0 ? url.substr(1) : url)
     }
     let option = {
       method,
       url,
       headers: {
-        Accept: "application/json",
-        Pragma: "no-cache",
-        "Cache-Control": "no-cache",
+        Accept: 'application/json',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache',
         Expires: 0,
-        "Content-Type": "application/json; charset=utf-8",
-        ...requstParams.extraHeaders,
-      },
-      dataType: "json",
-    };
+        'Content-Type': ContentType || 'application/json; charset=utf-8',
+        ...requstParams.extraHeaders
+      }
+    }
     // 参数赋值
     switch (method.toUpperCase()) {
-      case "GET":
-      case "DELETE":
-        option.params = body || {};
-        break;
-      case "POST":
-      case "PATCH":
-      case "PUT":
-        option.data = body || {};
-        break;
+      case 'GET':
+      case 'DELETE':
+        option.params = body || {}
+        break
+      case 'POST':
+      case 'PATCH':
+      case 'PUT':
+        option.data = body || {}
+        break
     }
 
     axios(option)
       .then(({ data }) => {
-        config.printLog && console.log("[request]", method, body, data);
-        resolve(data);
+        config.printLog && console.log('[request]', method, body, data)
+        resolve(data)
       })
       .catch((e) => {
         if (e.response) {
-          let { status, data } = e.response;
-          requstParams.errorHanlder(status, data);
+          let { status, data } = e.response
+          requstParams.errorHanlder(status, data)
         } else {
-          throw e;
+          throw e
         }
-      });
-  });
+      })
+  })
 }
